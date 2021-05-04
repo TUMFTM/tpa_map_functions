35a5a9f4614911f5f81b50760d284fd3320e5e77
Leonhard Hermansdorfer <leo.hermansdorfer@tum.de>
2021-05-04 14:51:44 +0200

    fix index error when s_actual is greater than s_max of race track
    pep8 stuff

--- tpa_map_functions/helperfuncs/calc_cosyidcs.py
+++ tpa_map_functions/helperfuncs/calc_cosyidcs.py
@@ -22,6 +22,16 @@ def calc_cosyidcs(sectionid: np.array,
     :rtype: tuple
     """
 
+    a = np.where(s_actual_m >= coordinates_sxy_m[-1, 0])[0]
+
+    if np.any(a):
+        idx_min = int(np.min(a) - 1)
+
+        print('WARNING: fix in calc_cosyidcs.py was triggered! '
+              + 's_max is: {}; requested s-coordinate(s): {}'.format(coordinates_sxy_m[-1, 0], s_actual_m[a]))
+
+        s_actual_m[a] = s_actual_m[idx_min]
+
     sectionid_change = np.concatenate((np.asarray([True]), np.diff(sectionid) > 0))
     coordinates_sxy_m = coordinates_sxy_m[sectionid_change]
 
@@ -32,3 +42,20 @@ def calc_cosyidcs(sectionid: np.array,
                    np.where(sectionid_change)[0][i + 1][:, np.newaxis]))
 
     return sectionid[sectionid_change][i], coordinates_sxy_m[i, 0], k
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# testing --------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+
+    sectionid = np.asarray([1, 1, 1, 2, 3, 3, 4, 5, 6, 6, 7])
+
+    coordinates_sxy_m = np.hstack((np.arange(11)[:, np.newaxis], np.zeros((11, 2))))
+    s_actual_m = np.linspace(1, 15.0, 11)
+
+    a, b, c = calc_cosyidcs(sectionid=sectionid,
+                            coordinates_sxy_m=coordinates_sxy_m,
+                            s_actual_m=s_actual_m)
+
+    pass
