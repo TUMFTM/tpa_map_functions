30a39b2ef50d15837ac73e673d105be879db3956
Leonhard Hermansdorfer <leo.hermansdorfer@tum.de>
2021-05-18 19:32:21 +0200

    release pypi version 0.16

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='tpa_map_functions',
-    version='0.15',
+    version='0.16',
     url='https://github.com/TUMFTM',
     author="Leonhard Hermansdorfer",
     author_email="leo.hermansdorfer@tum.de",
